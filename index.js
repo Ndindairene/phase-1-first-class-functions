ca5012dfd26c5c4314fdb3d72666739b62e8ef22
Ndindairene <irene.ndindaa@gmail.com>
2023-10-24 22:17:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-function receivesAFunction(callback){
-    callback();
-}
 
-function returnsANamedFunction(){
-    function namedFunction (){}
+function receivesAFunction(callback) {
+    callback();
+  }
+  function returnsANamedFunction() {
+    function namedFunction() {
+      console.log("This is a named function.");
+    }
     return namedFunction;
-}
-function returnsAnAnonymousFunction(){
-    return function(){}
+  }
+function returnsAnAnonymousFunction() {
+  return function() {
+    console.log("This is an anonymous function.");
+  };
 }
